91f32b6bb5e1587eaef3e1899a63ebf22effd1a0
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-02-19 09:14:36 -0500

    update tower hanoi

--- Tower_of_Hanoi.rb
+++ Tower_of_Hanoi.rb
@@ -34,97 +34,97 @@ tower.run(10)
 
 
 
-############ FIRST VERSION ##################
+########### FIRST VERSION ##################
 
-# class TowerOfHanoi
+class TowerOfHanoi
 
 
-#   attr_accessor :first_peg, :second_peg, :third_peg
+  attr_accessor :first_peg, :second_peg, :third_peg
 
-#   def initialize
-#     @first_peg = [1,2,3,4]
-#     @second_peg = []
-#     @third_peg = []
-#   end
+  def initialize
+    @first_peg = [1,2,3,4]
+    @second_peg = []
+    @third_peg = []
+  end
    
-#   # Move the disks from one peg to another following the rules of Hanoi.
-#   #
-#   # number_of_disks - the total number of disks
-#   # from - the starting peg (array)
-#   # to - the ending (goal) peg (array)
-#   # via - the remaining peg (array) (b in this case)
-#   #
+  # Move the disks from one peg to another following the rules of Hanoi.
+  #
+  # number_of_disks - the total number of disks
+  # from - the starting peg (array)
+  # to - the ending (goal) peg (array)
+  # via - the remaining peg (array) (b in this case)
+  #
 
-#   def one_disc(from, to) # gets two arrays as inputs
-#     to.unshift(from.shift) # take the top disc off of the from-peg, and put it a top the to-peg 
-#   end
+  def one_disc(from, to) # gets two arrays as inputs
+    to.unshift(from.shift) # take the top disc off of the from-peg, and put it a top the to-peg 
+  end
 
-#   def two_disc(from, to, via) # gets three arrays as inputs
-#     one_disc(from, via)
-#     one_disc(from, to)
-#     one_disc(via, to)
-#   end
+  def two_disc(from, to, via) # gets three arrays as inputs
+    one_disc(from, via)
+    one_disc(from, to)
+    one_disc(via, to)
+  end
 
-#   def three_disc(from, to, via) # gets three arrays as inputs
-#     two_disc(from, via, to)
-#     one_disc(from, to)
-#     two_disc(via, to, from)
-#   end
+  def three_disc(from, to, via) # gets three arrays as inputs
+    two_disc(from, via, to)
+    one_disc(from, to)
+    two_disc(via, to, from)
+  end
 
-#   def four_disc(from, to, via) # gets three arrays as inputs
-#     three_disc(from, via, to)
-#     one_disc(from, to)
-#     three_disc(via, to, from)
-#   end
+  def four_disc(from, to, via) # gets three arrays as inputs
+    three_disc(from, via, to)
+    one_disc(from, to)
+    three_disc(via, to, from)
+  end
 
-#   # # In case you want to have five discs ==> make sure to change array a to a = [1,2,3,4,5]
-#   # def five_disc(from, to, via) # gets three arrays as inputs
-#   #   four_disc(from, via, to)
-#   #   one_disc(from, to)
-#   #   four_disc(via, to, from)
-#   # end
+  # # In case you want to have five discs ==> make sure to change array a to a = [1,2,3,4,5]
+  # def five_disc(from, to, via) # gets three arrays as inputs
+  #   four_disc(from, via, to)
+  #   one_disc(from, to)
+  #   four_disc(via, to, from)
+  # end
 
-#   def move_disc(number_of_discs,from,to,via)
-#     # if number_of_discs == 1
-#     #   # move a disc from one array to another
-#     # else
-#     #   # make the recurssion happen here
-#     #   # you might need to call the method multiple times
-#     #   # to get things right.
-#     # end
-#     # to
+  def move_disc(number_of_discs,from,to,via)
+    # if number_of_discs == 1
+    #   # move a disc from one array to another
+    # else
+    #   # make the recurssion happen here
+    #   # you might need to call the method multiple times
+    #   # to get things right.
+    # end
+    # to
 
-#     case number_of_discs
-#     when 1
-#       one_disc(from, to)
-#       # puts "moving 1 disc from #{from} to #{to}"
-#     when 2
-#       two_disc(from, to, via)
-#       # puts "moving 2 discs from #{from} to #{to} via #{via}"
-#     when 3
-#       three_disc(from, to, via)
-#       # puts "moving 3 discs from #{from} to #{to} via #{via}"
-#     when 4
-#       four_disc(from, to, via)
-#       # puts "moving 4 discs from #{from} to #{to} via #{via}"
-#     end
+    case number_of_discs
+    when 1
+      one_disc(from, to)
+      # puts "moving 1 disc from #{from} to #{to}"
+    when 2
+      two_disc(from, to, via)
+      # puts "moving 2 discs from #{from} to #{to} via #{via}"
+    when 3
+      three_disc(from, to, via)
+      # puts "moving 3 discs from #{from} to #{to} via #{via}"
+    when 4
+      four_disc(from, to, via)
+      # puts "moving 4 discs from #{from} to #{to} via #{via}"
+    end
 
-#     # print out results after moving discs
-#     puts "First peg now has:"
-#     puts @first_peg
-#     puts "Second peg now has:"
-#     puts @second_peg
-#     puts "Third peg now has:"
-#     puts @third_peg
+    # print out results after moving discs
+    puts "First peg now has:"
+    puts @first_peg
+    puts "Second peg now has:"
+    puts @second_peg
+    puts "Third peg now has:"
+    puts @third_peg
 
-#   end
+  end
 
-# end
+end
  
-# # here we go!
-# tower = TowerOfHanoi.new
+# here we go!
+tower = TowerOfHanoi.new
 
-# tower.move_disc(4,tower.first_peg, tower.second_peg, tower.third_peg)
+tower.move_disc(4,tower.first_peg, tower.second_peg, tower.third_peg)
 
 
 
